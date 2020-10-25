2e04260b72eb438a31807543643bdac6a27c9479
yasirarism <55983182+yasirarism@users.noreply.github.com>
2020-10-25 19:36:45 +0700

    Update filters.py

--- emilia/modules/helper_funcs/filters.py
+++ emilia/modules/helper_funcs/filters.py
@@ -5,18 +5,6 @@ from emilia import SUPPORT_USERS, SUDO_USERS
 
 
 class CustomFilters(object):
-    class _Supporters(BaseFilter):
-        def filter(self, message: Message):
-            return bool(message.from_user and message.from_user.id in SUPPORT_USERS)
-
-    support_filter = _Supporters()
-
-    class _Sudoers(BaseFilter):
-        def filter(self, message: Message):
-            return bool(message.from_user and message.from_user.id in SUDO_USERS)
-
-    sudo_filter = _Sudoers()
-
     class _MimeType(BaseFilter):
         def __init__(self, mimetype):
             self.mime_type = mimetype
